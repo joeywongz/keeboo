5cd1d8665778710b8fddcf523d998f9207db2937
wangchong <370318843@qq.com>
2020-11-10 02:41:50 +0800

    新增EditLabel组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ Vue.use(VueRouter)
       component: Labels
     },
     {
-      path: '/labels/edit',
+      path: '/labels/edit/:id',
       component: EditLabels
     },
     {
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -2,8 +2,8 @@
   <div>
     <Layout>
       <ol class="tags">
-        <li v-for="tag in tags" :key="tag">
-          <span>{{ tag }}</span> <icon name="right" />
+        <li v-for="tag in tags" :key="tag.id">
+          <span>{{ tag.name }}</span> <icon name="right" />
         </li>
       </ol>
       <div class="createTag-wrapper">
