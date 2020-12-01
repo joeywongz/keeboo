25c80550f2c120f158a0b1a00d85a1a8f3c6fd97
wangchong <370318843@qq.com>
2020-12-02 00:11:56 +0800

    id生成后将_idMax值存入localstorage

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,7 +1,10 @@
-let id: number = parseInt(window.localStorage.getItem('_idMAX') || '0') || 0;
+//localstorage中的键值对总以字符串形式存在
+let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0; 
 
 function createId(){
-  return ++id;
+  id++;
+  window.localStorage.setItem('_idMax', id.toString())
+  return id;
 }
 
 export default createId;
