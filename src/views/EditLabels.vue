938cd36ebad0e27a6fa8a8c50eb29819efc369ec
wangchong <370318843@qq.com>
2020-12-01 21:30:52 +0800

    展示tag的name

--- src/views/EditLabels.vue
+++ src/views/EditLabels.vue
@@ -6,7 +6,10 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem fieldName="标签名" placeholder="请输入标签名" />
+      <FormItem fieldName="标签名" 
+                placeholder="请输入标签名"
+                :value="tag.name" 
+                />
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -24,13 +27,14 @@ import tagListModel from "@/models/tagListModel";
   components: { FormItem },
 })
 export default class EditLabels extends Vue {
+  tag?: {id: string; name: string} = undefined
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter((t) => t.id === id)[0];
     if (tag) {
-      console.log(tag);
+      this.tag = tag
     } else {
       this.$router.replace("/404"); //tag不存在则跳转404页面
     }
